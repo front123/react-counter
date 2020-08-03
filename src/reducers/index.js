c865101584bfa2d405b2a61dc2d87821e544db32
LIANGFR2 <372334264@qq.com>
2020-08-03 22:20:37 +0800

    [counter][front] feature:add counter actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 function counter(state = 0, action) {
     switch (action.type) {
-      case 'INCREMENT':
-        return state + 1
-      case 'DECREMENT':
-        return state - 1
-      default:
-        return state
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        case 'RESET':
+            return 0
+        default:
+            return state
     }
 }
 export default counter;
