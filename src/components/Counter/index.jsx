d3e6dd0cb724e40fb811d1d4e893c1f0e0264905
LIANGFR2 <372334264@qq.com>
2020-08-03 14:09:57 +0800

    create counter component

---
+++ src/components/Counter/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+class Counter extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            value: 0
+        }
+    }
+
+    onIncrease = ()=>{
+        this.setState((preState) => ({
+            value: preState.value + 1
+        }));
+    }
+
+    onReduce = ()=>{
+        this.setState((preState) => ({
+            value: preState.value - 1
+        }));
+    }
+
+    render(){
+        return <div>
+            <button onClick={this.onReduce}>-</button>
+            <mark>{this.state.value}</mark>
+            <button onClick={this.increase}>+</button>
+        </div>;
+    }
+}
+
+export default Counter;
