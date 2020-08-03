ba4fee2b39139c69aa3997894d1ab069ed82582f
LIANGFR2 <372334264@qq.com>
2020-08-03 22:21:25 +0800

    [counter][front] refactor: use redux dispatch to do increase or reduce

--- src/components/CounterGroup/index.jsx
+++ src/components/CounterGroup/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Counter from '../Counter';
 
-class CounterGroup extends React.Component{
-    constructor(props){
+class CounterGroup extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
             size: 0,
@@ -15,48 +15,67 @@ class CounterGroup extends React.Component{
         this.child.push(ref)
     }
 
-    handleResize = (event)=>{
-        let newSize = event.target.value?parseInt(event.target.value)>0?parseInt(event.target.value):0:0;
-        if (newSize < this.state.size){
-            for(let i=this.state.size; i>newSize; i--){
+    handleResize = (event) => {
+        let newSize = event.target.value ? parseInt(event.target.value) > 0 ? parseInt(event.target.value) : 0 : 0;
+        let sum = 0;
+        if (newSize < this.state.size) {
+            for (let i = this.state.size - 1; i >= newSize; i--) {
+                sum += this.child[i].state.value;
                 this.child.pop();
             }
-            this.child.forEach(element => {
-                element.reset();
-            });  
+            // this.child.forEach(element => {
+            //     element.reset();
+            // });  
         }
-        if (newSize !== this.state.size){
-            this.setState({
-                totalNumber: 0,
+        let temp = sum;
+        while(temp !== 0){
+            if (temp>0) {
+                this.props.store.dispatch({ type: 'DECREMENT' })
+                temp --;
+            }else{
+                this.props.store.dispatch({ type: 'INCREMENT' });
+                temp ++;
+            }
+        }
+        if (newSize === 0){
+            this.props.store.dispatch({ type: 'RESET' })
+        }
+        if (newSize !== this.state.size) {
+            this.setState((preState) => ({
+                totalNumber: preState.totalNumber - sum,
                 size: newSize
-            })
+            }))
         }
-        
+
     }
 
-    handleIncrease = ()=>{
-        this.setState((preState)=>({
+    handleIncrease = () => {
+        this.setState((preState) => ({
             totalNumber: preState.totalNumber + 1
         }))
     }
 
-    handleReduce = ()=>{
-        this.setState((preState)=>({
+    handleReduce = () => {
+        this.setState((preState) => ({
             totalNumber: preState.totalNumber - 1
         }))
     }
 
-    render(){
+    render() {
         const initArray = [...Array(this.state.size).keys()];
         return (
             <div>
+                <div>
+                    Counter Store:
+                    <span>{this.props.store.getState()}</span>
+                </div>
                 Counter Size:
-                <input type="Number" onBlur={this.handleResize}/>
+                <input type="Number" onBlur={this.handleResize} />
                 <div>
                     Total Number: <span>{this.state.totalNumber}</span>
                 </div>
                 {
-                    initArray.map(key => <Counter groupSize={this.state.size} onRef={this.onRef} onIncrease={this.handleIncrease} onReduce={this.handleReduce} key={key}/>)
+                    initArray.map(key => <Counter store={this.props.store} groupSize={this.state.size} onRef={this.onRef} onIncrease={this.handleIncrease} onReduce={this.handleReduce} key={key} />)
                 }
             </div>
         );
